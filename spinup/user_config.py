583cd32eb53e7e1f8db8fa0e2bdba15d14b7a52b
hepengli <51325455+hepengli@users.noreply.github.com>
2022-08-09 14:33:06 -0400

    Update user_config.py

--- spinup/user_config.py
+++ spinup/user_config.py
@@ -9,7 +9,8 @@ DEFAULT_BACKEND = {
     'ppo': 'pytorch',
     'ddpg': 'pytorch',
     'td3': 'pytorch',
-    'sac': 'pytorch'
+    'sac': 'pytorch',
+    'gpo': 'pytorch'
 }
 
 # Where experiment outputs are saved by default:
